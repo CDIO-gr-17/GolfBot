d59fb9bd7be3c70ce8a3d6cd3ee97cd9c0c38510
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2024-04-03 10:00:06 +0200

    Wi-Fi control. DONE

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
+import socket
 
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
@@ -14,16 +15,37 @@ right_motor = Motor(Port.D)
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-# Go forward and backwards for one meter.
-robot.straight(1000)
-ev3.speaker.beep()
+# Create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-robot.straight(-1000)
-ev3.speaker.beep()
+# Get local machine name
+host = "192.168.8.111"
+port = 9999
 
-# Turn clockwise by 360 degrees and back again.
-robot.turn(360)
-ev3.speaker.beep()
+# Bind to the port
+serversocket.bind((host, port))
 
-robot.turn(-360)
-ev3.speaker.beep()
+# Queue up to 5 requests
+serversocket.listen(5)
+
+while True:
+    # Establish a connection
+    clientsocket,addr = serversocket.accept()
+    
+    # Receive the command
+    command = clientsocket.recv(1024).decode('utf-8')
+    
+    if command == 'FORWARD':
+        # Code to move the robot forward
+        # Go forward for one meter.
+        robot.straight(1000)
+        ev3.speaker.beep()
+        pass
+    elif command == 'BACKWARD':
+        # Go backwards for one meter.
+        # Code to move the robot backward
+        robot.straight(-1000)
+        ev3.speaker.beep()
+        pass
+
+    clientsocket.close()
