2a25ed4961ffe733a83f6789ec0c4eac26350f60
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2024-06-07 14:24:41 +0200

    Fixed transfer of path as .json

--- src/on_computer/ComputerMain.py
+++ src/on_computer/ComputerMain.py
@@ -4,21 +4,25 @@ from PathfindingAlgorithm import grid, start, end, Node
 import json
 
 # Creates a socket object, and established a connection to the robot
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "192.168.8.111"
 port = 9999
 
-socket.connect((host, port))
+client_socket.connect((host, port))
+
+command = 'PATH'
+client_socket.sendall(command.encode('utf-8'))
 
 # Send the path to the robot
 path = PathfindingAlgorithm.a_star(grid, start, end)
 path_as_dictionaries = [{'x': node.x, 'y': node.y} for node in path]
 path_as_json = json.dumps(path_as_dictionaries)
 
-command = 'PATH'
-socket.send(command.encode('utf-8'))
-socket.send(path_as_json.encode('utf-8'))
+json_length = len(path_as_json)
+client_socket.sendall(json_length.to_bytes(4, 'big'))
+
+client_socket.sendall(path_as_json.encode('utf-8'))
 
 # Close the connection
-socket.close()
+client_socket.close()
--- src/on_ev3/RobotBuilder.py
+++ src/on_ev3/RobotBuilder.py
@@ -2,89 +2,87 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-import Heading
 import math
+from Heading import Heading
 
 class Robot:
-
+    
     # Initialize the EV3 Brick.
-    ev3 = EV3Brick()
+    def __init__(self):
+        self.ev3 = EV3Brick()
 
-    # Initialize the motors.
-    left_motor = Motor(Port.D)
-    right_motor = Motor(Port.A)
+        # Initialize the motors.
+        self.left_motor = Motor(Port.D)
+        self.right_motor = Motor(Port.A)
 
-    # Initialize the drive base.
-    WHEEL_DIAMETER = 55
-    AXLE_TRACK = 95
-    robot = DriveBase(left_motor, right_motor, WHEEL_DIAMETER, AXLE_TRACK)
-    #robot.settings(1000,100,90,45)
-    #settings(straight_speed, straight_acceleration, turn_rate, turn_acceleration)
+        # Initialize the drive base.
+        self.WHEEL_DIAMETER = 55
+        self.AXLE_TRACK = 95
+        self.robot = DriveBase(self.left_motor, self.right_motor, self.WHEEL_DIAMETER, self.AXLE_TRACK)
 
-    GRID_DISTANCE = 50
+        self.GRID_DISTANCE = 50
 
-    def turnRight():
-        robot.turn(45)
+    def turnRight(self):
+        self.robot.turn(45)
 
-    def turnLeft():
-        robot.turn(-45)
+    def turnLeft(self):
+        self.robot.turn(-45)
 
-    def moveForward():
-        robot.straight(GRID_DISTANCE)
+    def moveForward(self):
+        self.robot.straight(self.GRID_DISTANCE)
 
-    def moveForwardCross():
-        robot.straight(GRID_DISTANCE*1.414)
+    def moveForwardCross(self):
+        self.robot.straight(self.GRID_DISTANCE * 1.414)
 
-    def moveBackward():
-        robot.straight(-100)
+    def moveBackward(self):
+        self.robot.straight(-100)
 
-    def moveToNeighbor(target : Heading , currentHeading: Heading):
+    def moveToNeighbor(self, target: Heading, currentHeading: Heading):
         while currentHeading != target:
             if currentHeading < target:
-                turnRight()
+                self.turnRight()
                 currentHeading = currentHeading + 1
             else:
-                turnLeft()
-                currentHeading = currentHeading - 1
+                self.turnLeft()
+                currentHeading = currentHeading +1
         if target % 2 == 0:
-            moveForwardCross()
+            self.moveForwardCross()
         else:
-            moveForward()
+            self.moveForward()
         return currentHeading
 
-
-    def moveToPoint(x: int, y: int, currentX: int, currentY: int, currentHeading: Heading):
+    def moveToPoint(self, x: int, y: int, currentX: int, currentY: int, currentHeading: Heading):
         while currentX != x or currentY != y:
             if x > currentX:
                 if y > currentY:
-                    currentHeading = moveToNeighbor(Heading.SOUTHEAST, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.SOUTHEAST, currentHeading)
                     currentX += 1
                     currentY += 1
                 elif y < currentY:
-                    currentHeading = moveToNeighbor(Heading.NORTHEAST, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.NORTHEAST, currentHeading)
                     currentX += 1
                     currentY -= 1
                 else:
-                    currentHeading = moveToNeighbor(Heading.EAST, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.EAST, currentHeading)
                     currentX += 1
             elif x < currentX:
                 if y > currentY:
-                    currentHeading = moveToNeighbor(Heading.SOUTHWEST, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.SOUTHWEST, currentHeading)
                     currentX -= 1
                     currentY += 1
                 elif y < currentY:
-                    currentHeading = moveToNeighbor(Heading.NORTHWEST, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.NORTHWEST, currentHeading)
                     currentX -= 1
                     currentY -= 1
                 else:
-                    currentHeading = moveToNeighbor(Heading.WEST, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.WEST, currentHeading)
                     currentX -= 1
             else:
                 if y > currentY:
-                    currentHeading = moveToNeighbor(Heading.SOUTH, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.SOUTH, currentHeading)
                     currentY += 1
                 elif y < currentY:
-                    currentHeading = moveToNeighbor(Heading.NORTH, currentHeading)
+                    currentHeading = self.moveToNeighbor(Heading.NORTH, currentHeading)
                     currentY -= 1
             print("ran")
         return currentHeading
