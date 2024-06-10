2b3552fe8b265976e20488711b0cad8453ca8c27
Philip <philipmuff@gmail.com>
2024-06-10 15:51:54 +0200

    Improved write_pixel_grid 1000x and refactored it to create_combined_grid

--- src/on_computer/computer_vision/ComputerVision.py
+++ src/on_computer/computer_vision/ComputerVision.py
@@ -4,10 +4,10 @@ import numpy as np
 
 
 #Kamera 0
-red_lower_1 = np.array([0, 100, 20], dtype="uint8")
+red_lower_1 = np.array([0, 200, 20], dtype="uint8")
 red_upper_1 = np.array([5, 255, 255], dtype="uint8")
 
-red_lower_2 = np.array([170, 100, 20], dtype="uint8")
+red_lower_2 = np.array([170, 200, 20], dtype="uint8")
 red_upper_2 = np.array([180, 255, 255], dtype="uint8")
 
 orange_lower = np.array([15, 100, 20], dtype="uint8")
@@ -44,68 +44,30 @@ def get_robot_tail(mask_blue):
 
 
 #This should be rewritten to use np.argwhere instead of nested for loops
-def write_pixel_grid(mask_red, mask_orange, mask_white, mask_green, matrix):
-    obstacle_grid = matrix.copy()
-    balls_grid = matrix.copy()
-    goal_grid = matrix.copy()
+def create_combined_grid(mask_red, mask_orange, mask_white, matrix):
+    obstacle_coordinates = set(map(tuple, np.argwhere(mask_red != 0)))
+    ball_coordinates = set(map(tuple, np.argwhere(np.logical_or(mask_orange != 0, mask_white != 0))))
     combined_grid = matrix.copy()
-    rows, cols = mask_red.shape
-    for i in range(rows):
-        for j in range(cols):
 
-            #Obstacle
-            red_pixel_value = mask_red[i, j]
-            #binary_value = 0 if pixel_value == 0 else 1 # Calculate binary value
-            obstacle_binary_value = 0
-            if red_pixel_value == 0:
-                obstacle_binary_value = 0
+    for i in range(combined_grid.shape[0]):
+        for j in range(combined_grid.shape[1]):
+            if (i, j) in obstacle_coordinates:
+                combined_grid[i, j] = 1
+            elif (i, j) in ball_coordinates:
+                combined_grid[i, j] = 2
             else:
-                obstacle_binary_value = 1
-            obstacle_grid[i][j] = obstacle_binary_value
+                combined_grid[i, j] = 0
+        
+    #np.savetxt('combined_grid.txt', combined_grid, fmt='%d')
 
-            #Balls
-            orange_pixel_value = mask_orange[i, j]
-            white_pixel_value = mask_white[i, j]
-            balls_binary_value = 0
-            if orange_pixel_value != 0 or white_pixel_value != 0:
-                balls_binary_value = 1
-            balls_grid[i][j] = balls_binary_value
-
-            #Goal
-            green_pixel_value = mask_green[i, j]
-            goal_binary_value = 0
-            if green_pixel_value == 0:
-                goal_binary_value = 0
-            else:
-                goal_binary_value = 1
-            goal_grid[i][j] = goal_binary_value
-
-            # Set the values in the combined grid
-            if obstacle_grid[i][j] == 1:
-                combined_grid[i][j] = 1
-            elif balls_grid[i][j] == 1:
-                combined_grid[i][j] = 2
-
-    #print(algorithm.ball_detector(balls_grid))
-
-    # Save the grids to files - Unneccessary should be removed, but might be nice for visualization and debugging
-   # with open("grid_output/obstacle.txt", "w") as file:
-   #     np.savetxt(file, obstacle_grid, fmt="%.0f")
-    #with open("grid_output/balls.txt", "w") as file:
-   #     np.savetxt(file, balls_grid, fmt="%.0f")
-   # with open("grid_output/goal.txt", "w") as file:
-   #     np.savetxt(file, goal_grid, fmt="%.0f")
-    #with open("grid_output/combined_grid.txt", "w") as file:
-     #   np.savetxt(file, combined_grid, fmt="%.0f")
-
-    return combined_grid # Returns the combined grid
+    return combined_grid
 
 def display_grid(mask):
     upscaled_resized_frame = cv.resize(mask, (x*7, y*7), interpolation=cv.INTER_NEAREST)
     cv.imshow('ImageWindow', upscaled_resized_frame)
 
 def get_grid():
-    video_capture = cv.VideoCapture(0)
+    video_capture = cv.VideoCapture(1, cv.CAP_DSHOW)
 
     while True:
         ret, frame = video_capture.read()
@@ -140,8 +102,8 @@ def get_grid():
             mask_orange = cv.dilate(mask_orange, ball_kernel)
             mask_red = cv.dilate(mask_red,obstacle_kernel)
 
-            upscaled_resized_frame = cv.resize(mask_white, (x, y), interpolation=cv.INTER_NEAREST)
-            #grid = write_pixel_grid(mask_red, mask_orange, mask_white, mask_green, mask_grid)
+            upscaled_resized_frame = cv.resize(mask_white, (x*2, y*2), interpolation=cv.INTER_NEAREST)
+            grid = create_combined_grid(mask_red, mask_orange, mask_white, mask_grid)
         #display_grid(frame)
         cv.imshow('ImageWindow', upscaled_resized_frame)
         if cv.waitKey(1) & 0xFF == ord('q'): break
@@ -157,7 +119,7 @@ get_grid()
 
 def get_robot_position_and_heading():
     heading = 'NORTH'
-    grid  = write_pixel_grid(mask_red, mask_orange, mask_white, mask_green, mask_grid)
+    grid  = create_combined_grid(mask_red, mask_orange, mask_white, mask_grid)
     blue_square = grid[1][1]
     green_square = grid[1][2]
     if (blue_square[0] > green_square[0]):
