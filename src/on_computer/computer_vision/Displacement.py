ac7865907da9cb5e82c180ef2fb0c640d3238e50
EsbenSE <s224555@student.dtu.dk>
2024-06-11 10:26:10 +0200

    findangle from centre to robot

--- src/on_computer/computer_vision/Displacement.py
+++ src/on_computer/computer_vision/Displacement.py
@@ -27,11 +27,17 @@ def find_angle_to_field(grid, field):
     angle = math.atan(camera_height/distance)
     return angle
 
+def find_angle_from_centre_to_robot(grid, robot_field_position):
+    middle = find_middle(grid)
+    angle = math.atan((robot_field_position[0]-middle[0])/(robot_field_position[1]-middle[1]))
+    return angle
+
 #Find the displacement of the robot, given the field position of the robot
 def find_displacement_of_robot(grid, robot_field_position):
     angle = find_angle_to_field(grid, robot_field_position)
     displacement = robot_height/math.tan(angle)
     return displacement
+
 
 print(find_middle(grid))
 robot_field_position = (0/grid_size,150/grid_size)
