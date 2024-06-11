96712f3b6c2d7cba139a94a008e66633c15618ac
Philip <philipmuff@gmail.com>
2024-06-11 13:21:32 +0200

    Change x and y values and made combined grid show correctly

--- src/on_computer/computer_vision/ComputerVision.py
+++ src/on_computer/computer_vision/ComputerVision.py
@@ -25,7 +25,7 @@ blue_upper = np.array([110, 255, 255], dtype="uint8")
 
 x = 250
 y = 140
-resolution = (x, y) # Det skal vel ikke være kvadratisk?
+resolution = (x, y)
 mask_grid = np.zeros((resolution))
 
 def get_robot_head(mask_green):
@@ -45,7 +45,7 @@ def get_robot_tail(mask_blue):
 def create_combined_grid(mask_red, mask_orange, mask_white, matrix):
     obstacle_coordinates = set(map(tuple, np.argwhere(mask_red != 0)))
     ball_coordinates = set(map(tuple, np.argwhere(np.logical_or(mask_orange != 0, mask_white != 0))))
-    combined_grid = matrix.copy()
+    combined_grid = mask_red.copy()
 
     for i in range(combined_grid.shape[0]):
         for j in range(combined_grid.shape[1]):
@@ -67,52 +67,41 @@ def display_grid(mask):
 def get_grid():
     video_capture = cv.VideoCapture(1, cv.CAP_DSHOW)
 
-    #while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        print("No frame")
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            print("Camera not detected")
 
-    #Make the frame blurry
-    blur_frame = cv.GaussianBlur(frame, (17, 17), 0)
-    #Make the frame hsv
-    hsv_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2HSV)
-    #Make the frame
-    resized_frame = cv.resize(hsv_frame, resolution, interpolation=cv.INTER_NEAREST)
+        elif ret:
+            blur_frame = cv.GaussianBlur(frame, (17, 17), 0) #Add blur
 
-    # Create masks for red, green, blue
-    mask_red_1 = cv.inRange(resized_frame, red_lower_1, red_upper_1)
-    mask_red_2 = cv.inRange(resized_frame, red_lower_2, red_upper_2)
-    mask_red = cv.bitwise_or(mask_red_1, mask_red_2)
-    mask_green = cv.inRange(resized_frame, green_lower, green_upper)
-    mask_blue = cv.inRange(resized_frame, blue_lower, blue_upper)
-    mask_orange = cv.inRange(resized_frame, orange_lower, orange_upper)
-    mask_white = cv.inRange(resized_frame, white_lower, white_upper)
+            hsv_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2HSV) #Convert from RGB to HSV
 
-    ret, mask = cv.threshold(resized_frame, 200, 255, cv.THRESH_BINARY) #Hvad gør den her linje?
-    if ret:
-        #kernel = np.ones((5, 5), np.uint8) #den binære repræsentation af et billede
-        #mask_cleaned = cv.morphologyEx(mask_to_use, cv.MORPH_OPEN, kernel) #mask isolere arealer i et billede
-        #mask_cleaned = cv.morphologyEx(mask_cleaned, cv.MORPH_CLOSE, kernel) #siger nej tak til farver og siger ja tak til HVID
-        #contours, hierarchy = cv.findContours(mask_to_use, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) #hierarchy er moralsk support her
-        obstacle_kernel = np.ones((15, 15), np.uint8)
-        ball_kernel = np.ones((3, 3), np.uint8)
-        mask_white = cv.dilate(mask_white, ball_kernel)
-        mask_orange = cv.dilate(mask_orange, ball_kernel)
-        mask_red = cv.dilate(mask_red,obstacle_kernel)
+            resized_frame = cv.resize(hsv_frame, resolution, interpolation=cv.INTER_NEAREST) #Apply the resolution specified
 
-        upscaled_resized_frame = cv.resize(mask_white, (x*2, y*2), interpolation=cv.INTER_NEAREST)
-        grid = create_combined_grid(mask_red, mask_orange, mask_white, mask_grid)
-    #display_grid(frame)
-    cv.imshow('ImageWindow', upscaled_resized_frame)
-    #if cv.waitKey(1) & 0xFF == ord('q'): break
+            # Create masks for red, green, blue, orange and white
+            mask_red_1 = cv.inRange(resized_frame, red_lower_1, red_upper_1)
+            mask_red_2 = cv.inRange(resized_frame, red_lower_2, red_upper_2)
+            mask_red = cv.bitwise_or(mask_red_1, mask_red_2)
+            mask_green = cv.inRange(resized_frame, green_lower, green_upper)
+            mask_blue = cv.inRange(resized_frame, blue_lower, blue_upper)
+            mask_robot = cv.bitwise_or(mask_green, mask_blue) #Combine green and blue masks, doesn't work optimally
+            mask_orange = cv.inRange(resized_frame, orange_lower, orange_upper)
+            mask_white = cv.inRange(resized_frame, white_lower, white_upper)
+            obstacle_kernel = np.ones((15, 15), np.uint8)
+            ball_kernel = np.ones((3, 3), np.uint8)
+            mask_white = cv.dilate(mask_white, ball_kernel)
+            mask_orange = cv.dilate(mask_orange, ball_kernel)
+            mask_red = cv.dilate(mask_red,obstacle_kernel)
+            grid = create_combined_grid(mask_red, mask_orange, mask_white, mask_grid)
+            #display_grid(frame)
+            cv.imshow('ImageWindow', mask_robot)
+            if cv.waitKey(1) & 0xFF == ord('q'): break
     video_capture.release()
     cv.destroyAllWindows()
     return grid
 
-
-
 get_grid()
-#if cv.waitKey(1) & 0xFF == ord('q'): break
 
 
 def get_robot_position_and_heading():
