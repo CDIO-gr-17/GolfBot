3ec8573bb089243552480629af6c68a1626434c3
Jakob-SA <agergaardskovjakob@gmail.com>
2024-06-23 16:06:09 +0200

    Should be a fix for closing the socket correctly on error

--- src/on_ev3/EV3Main.py
+++ src/on_ev3/EV3Main.py
@@ -9,77 +9,89 @@ from RobotBuilder import Robot
 print('running...')
 print(sys.version_info)  # Check python version
 
-# Setup the connection
-s = EV3Connector.establish_socket()
-robot = Robot()
-clientsocket, adress = s.accept()
-print('Connection established')
+clientsocket = None
+s = None
+
+try:
+    # Setup the connection
+    s = EV3Connector.establish_socket()
+    robot = Robot()
+    clientsocket, adress = s.accept()
+    print('Connection established')
 
 
-def recv_all(sock, length):  # Helper function to receive exactly 'length' bytes from 'sock'
-    data = bytearray()
-    while len(data) < length:
-        packet = sock.recv(length - len(data))
-        if not packet:
-            return None
-        data.extend(packet)
-    return data
+    def recv_all(sock, length):  # Helper function to receive exactly 'length' bytes from 'sock'
+        data = bytearray()
+        while len(data) < length:
+            packet = sock.recv(length - len(data))
+            if not packet:
+                return None
+            data.extend(packet)
+        return data
 
 
-while True:
-    print("Awaiting command...")
-    # Receive a command
-    robot.buffer = clientsocket.recv(4).decode('utf-8').strip()
+    while True:
+        print("Awaiting command...")
+        # Receive a command
+        robot.buffer = clientsocket.recv(4).decode('utf-8').strip()
 
-    if 'PATH' in robot.buffer:
-        print('Recieved command: PATH')
+        if 'PATH' in robot.buffer:
+            print('Recieved command: PATH')
 
-        initial_heading = int(clientsocket.recv(3).decode('utf-8').rstrip('\x00'))
-        print('Initial heading: ', str(initial_heading))
-        data_length = clientsocket.recv(4)
+            initial_heading = int(clientsocket.recv(3).decode('utf-8').rstrip('\x00'))
+            print('Initial heading: ', str(initial_heading))
+            data_length = clientsocket.recv(4)
 
-        if data_length is not None:
-            length = int.from_bytes(data_length, 'big')
-            path_data = recv_all(clientsocket, length).decode('utf-8')
-            path = [(node[0], node[1]) for node in json.loads(path_data)]
+            if data_length is not None:
+                length = int.from_bytes(data_length, 'big')
+                path_data = recv_all(clientsocket, length).decode('utf-8')
+                path = [(node[0], node[1]) for node in json.loads(path_data)]
 
-            initialX = path[0][0]
-            initialY = path[0][1]
+                initialX = path[0][0]
+                initialY = path[0][1]
 
-            path_length = len(path)
-            robot.move_through_path(path[0], path[path_length-1], initial_heading, path, clientsocket)
-        while 'PATH' in robot.buffer:
-            robot.buffer = robot.buffer.replace('PATH', '')
+                path_length = len(path)
+                robot.move_through_path(path[0], path[path_length-1], initial_heading, path, clientsocket)
+            while 'PATH' in robot.buffer:
+                robot.buffer = robot.buffer.replace('PATH', '')
 
-    if 'PICK' in robot.buffer:
-        print('Recieved command: PICK')
+        if 'PICK' in robot.buffer:
+            print('Recieved command: PICK')
 
-        distance_to_ball = float(clientsocket.recv(40).decode('utf-8').rstrip('\x00'))
-        heading_to_ball = int(clientsocket.recv(3).decode('utf-8').rstrip('\x00'))
-        print('The robot should be at', heading_to_ball, 'degrees')
-        robot.current_heading = int(clientsocket.recv(3).decode('utf-8').rstrip('\x00'))
-        print('The robot is at', robot.current_heading, 'degrees')
-        print('Picking up ball')
-        robot.pickup_ball(distance_to_ball, heading_to_ball)
-        clientsocket.send('STOPPED'.encode('utf-8'))
-        robot.step = 0
-        while 'PICK' in robot.buffer:
-            robot.buffer = robot.buffer.replace('PICK', '')
+            distance_to_ball = float(clientsocket.recv(40).decode('utf-8').rstrip('\x00'))
+            heading_to_ball = int(clientsocket.recv(3).decode('utf-8').rstrip('\x00'))
+            print('The robot should be at', heading_to_ball, 'degrees')
+            robot.current_heading = int(clientsocket.recv(3).decode('utf-8').rstrip('\x00'))
+            print('The robot is at', robot.current_heading, 'degrees')
+            print('Picking up ball')
+            robot.pickup_ball(distance_to_ball, heading_to_ball)
+            clientsocket.send('STOPPED'.encode('utf-8'))
+            robot.step = 0
+            while 'PICK' in robot.buffer:
+                robot.buffer = robot.buffer.replace('PICK', '')
 
-    if 'GOAL' in robot.buffer:
-        print('Recieved command: GOAL')
+        if 'GOAL' in robot.buffer:
+            print('Recieved command: GOAL')
 
-        data_length = clientsocket.recv(4)
-        if data_length is not None:
-            length = int.from_bytes(data_length, 'big')
-            path_data = recv_all(clientsocket, length).decode('utf-8')
-            path = [(node[0], node[1]) for node in json.loads(path_data)]
+            data_length = clientsocket.recv(4)
+            if data_length is not None:
+                length = int.from_bytes(data_length, 'big')
+                path_data = recv_all(clientsocket, length).decode('utf-8')
+                path = [(node[0], node[1]) for node in json.loads(path_data)]
 
-            initialX = path[0][0]
-            initialY = path[0][1]
+                initialX = path[0][0]
+                initialY = path[0][1]
 
-            path_length = len(path)
-            robot.move_through_path(path[0], path[path_length-1], 0, path, clientsocket)
-            robot.deposit()
-        while 'GOAL' in robot.buffer:
-            robot.buffer = robot.buffer.replace('GOAL', '')
+                path_length = len(path)
+                robot.move_through_path(path[0], path[path_length-1], 0, path, clientsocket)
+                robot.deposit()
+            while 'GOAL' in robot.buffer:
+                robot.buffer = robot.buffer.replace('GOAL', '')
+
+except Exception as e:
+    print('An error occurred:', e)
+finally:
+    if clientsocket:
+        clientsocket.close()
+    if s:
+        s.close()
